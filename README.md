f2f8b54c32749af33e797c3ac67bccf8b970ed4d
struggzard <59175252+struggzard@users.noreply.github.com>
2020-02-22 18:09:34 +0200

    browser addon source added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# firefox-extension-sample
+# Firefox Extension Sample
+
+A simple browser extension to demonstrate native notifications.
+
+## Media
+* baseline_notification_important_black_48dp.png - https://material.io/resources/icons/
---
+++ just-sample.js
@@ -0,0 +1,17 @@
+const createNotification = () => {
+    browser.notifications.create({
+        "type": "basic",
+        "iconUrl": browser.extension.getURL("icons/baseline_notification_important_black_48dp.png"),
+        "title": "Hi!",
+        "message": "Just test content :)"
+    });
+};
+
+const timeout = 30000; // 30s
+
+const mainLoop = () => setTimeout(() => {
+    createNotification();
+    mainLoop();
+}, 30000);
+
+mainLoop();
